3b0c4a575f3279bbeb6927de578bf886ad86ca24
Mercer Frey <deadharmony6969@gmail.com>
2018-09-18 12:02:47 +0300

    каруселька

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,28 @@
 $(function() {
 
-	// Custom JS
-
+	$(document).ready(function(){
+ 		$(".owl-carousel").owlCarousel({
+ 			loop: true,
+		    smartSpeed: 700,
+		    navText: ['<i class="fal fa-angle-left"></i>', '<i class="fal fa-angle-right"></i>'],
+		    margin: 10,
+		    freeDrag: true,
+		    dotsEach: true,
+		    responsiveClass: !0,
+		    responsive: {
+		        0: {
+		            items: 2
+		        },
+		        768: {
+		            items: 3
+		        },
+		        992: {
+		            items: 4 
+		        },
+		        1200: {
+		            items: 5
+		        }
+		    } 	 	
+ 		});
+	});
 });
